5b981a2afaab257633a0c6a0ca3dc8beb9d36b4d
Dario Di Carlo <dario@bricks.sh>
2024-08-01 11:37:34 +0200

    added Inter as main font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,30 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
+
+import "@/styles/globals.css"
+import { Inter as FontSans } from "next/font/google"
+
+import { cn } from "@/lib/utils"
+
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+})
+
+export default function RootLayout({ children }: RootLayoutProps) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <head />
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable
+        )}
+      >
+        ...
+      </body>
+    </html>
+  )
+}
 
 const inter = Inter({ subsets: ["latin"] });
 
